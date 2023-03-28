fd472044c6ab44a506cd343c67b2645b49536c6d
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-03-28 15:06:17 +0100

    Adding the methods

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add() {
+        return a + b;
+    }
+
+    public int subtract() {
+        return a - b;
+    }
+
+    public int multiply() {
+        return a * b;
+    }
+
+    public double divide() {
+        return (double) a / b;
+    }
+
+
+
+}
